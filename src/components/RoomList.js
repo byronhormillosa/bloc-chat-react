21f04fa16bcee15789d3821cd3d0e0f017b633b7
byronhormillosa <byronhormillosa@gmail.com>
2018-07-02 02:39:58 -0500

    checkpoint 2 render chat room list

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -19,6 +19,7 @@ class RoomList extends Component {
     render() {
         return (
           <div className="roomlist">
+            <h1>Chat Room List</h1>
             <ul>
               {this.state.rooms.map( (room, index) => (
                 <li key={index}> {room.name} </li>
